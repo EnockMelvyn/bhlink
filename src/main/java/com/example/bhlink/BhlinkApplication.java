832c4d8989585a1fe46cb402dfe02bb78b88e9fd
Enock Melvyn AKA <akmelvyn@gmail.com>
2022-07-23 05:09:12 +0000

    Premier commit après initialisation du projet

---
+++ src/main/java/com/example/bhlink/BhlinkApplication.java
@@ -0,0 +1,13 @@
+package com.example.bhlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BhlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BhlinkApplication.class, args);
+	}
+
+}
